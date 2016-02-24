34c87374c7f1298b9bbbdd741b8d97edf4844c8d
adgekko <nhviktor@gmail.com>
2016-02-24 19:51:31 +0100

    Az aktív menüelem kiemelése

---
+++ src/main/java/webalapu/backing/MenuBacking.java
@@ -0,0 +1,27 @@
+package webalapu.backing;
+
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+
+@Named
+@RequestScoped
+public class MenuBacking {
+
+    public String getPath(String fileName) {
+        return "/public/pages/" + fileName;
+    }
+
+    public String isActive(String fileName) {
+        HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String path = req.getServletPath();
+
+        if (path.substring(path.lastIndexOf("/") + 1).equals(fileName)){
+            return " active";
+        }
+
+        return "";
+    }
+}
